ce4c1995117bb8c51dbe98d19d99534ad3f74ebf
Samayas <info@samayas.eu>
2025-07-15 18:44:06 +0200

    Add Mobile Emulator Test Size

---
+++ Web/MobileEmulator/MobileEmulatorTestSite/Program.cs
@@ -0,0 +1,38 @@
+
+namespace MobileEmulatorTestSite
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseStatusCodePagesWithReExecute("/Home/Error");
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
---
+++ Web/MobileEmulator/MobileEmulatorTestSite/ViewModels/BaseViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MobileEmulatorTestSite.ViewModels
+{
+    public class BaseViewModel
+    {
+        public string Title { get; set; } = "Mobile Emulator Test Size";
+    }
+}
