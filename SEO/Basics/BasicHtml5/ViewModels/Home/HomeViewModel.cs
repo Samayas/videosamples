a6986027ee3cb4252be3fbb77e8256133ec04fa4
Samayas <info@samayas.eu>
2025-11-05 20:57:05 +0100

    update

---
+++ SEO/Basics/BasicHtml5/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicHeadings.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public HomeViewModel(string title, string metaDescription, string keywords) : base(title, metaDescription, keywords) 
+        {
+        }
+    }
+}
